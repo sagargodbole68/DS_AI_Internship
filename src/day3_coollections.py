f64562327573787aa8623d2359a70193551df2ed
sagar <sagargodbole68@gmail.com>
2026-02-04 12:55:35 +0530

    day 3

---
+++ src/day3_coollections.py
@@ -0,0 +1,24 @@
+#task1
+
+inventory = ["Apples","Bananas","Oranges","Dates"]
+inventory.append("Oranges")
+inventory.remove("Bananas")
+inventory.sort()
+print(inventory)
+
+#task2
+temperature_readings = [72, 75, 78, 79, 73, 77, 74]
+print(temperature_readings[0], temperature_readings[-1])
+print(temperature_readings[3:6])
+print(temperature_readings[-3])
+
+
+      
+#task3
+screen_res = (1920, 1080)
+print(f"Current screen resolution: {screen_res[0]} x {screen_res[1]}")
+screen_res = (1280, 1080)
+print("tuples file cannot be changed")
+
+
+

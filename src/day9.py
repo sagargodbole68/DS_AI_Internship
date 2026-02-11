2523f20827bd0375b0da49adb55c3958d2a3a9f7
sagargodbole68 <sagargodbole68@gmail.com>
2026-02-11 15:53:23 +0530

    day 9

--- src/day9.py
+++ src/day9.py
@@ -1,3 +1,5 @@
+#task 1
+
 import numpy as np
 import pandas as pd 
 values = pd.Series(data=[700,150,300], index = ['Laptop','Mouse','keyboard'])
@@ -7,9 +9,19 @@ print("first two products:",values[0:2])
  
 
 #task2
+
 grades = pd.Series( [85,None,92,45, None,78,55])
 grades.isnull()
 grades.fillna(0)
 print(grades[grades > 60])
-swed
+
+#task3
+
+usernames = pd.Series( [' Alice ', 'bOB', ' Charlie_Data ', 'daisy'])
+usernames.str.strip()
+usernames.str.lower()
+low = usernames.str.lower()
+low.str.contains(('a'))
+print(low[low.str.contains('a')])
+    
 

56f0339f8f747f299df4b45469506174133edce7
sagargodbole68 <sagargodbole68@gmail.com>
2026-02-13 13:32:48 +0530

    day11

---
+++ src/day11.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+plt.title("Monthly Revenue Growth")
+months = [1,2,3,4,5]
+revenue = [2000,4001,5002,4343,1232]
+plt.xlabel("months")
+plt.ylabel("revenue")
+plt.plot(months,revenue)
+plt.show()
